ac74a329b39d12100e352b4effc31826d3e829d0
Sara Nadifi <nadifisara@gmail.com>
2025-05-12 16:20:07 +0200

    sara

--- src/pages/temp/Sara.js
+++ src/pages/temp/Sara.js
@@ -1,14 +1,21 @@
-import CarouselSpecial from "../../components/carouselSpecial/carouselSpecial";
+import Btn from "../../components/Button/Btn";
+import Features from "../../components/Features/Features";
+import MyNavbar from "../../components/MyNavbar/MyNavbar";
+
 function Sara() {
-
-
     return (
         <div className="main">
-               <h1>Sara</h1>
-               <CarouselSpecial />
-        </div>
+         
+            <MyNavbar />
 
-    )
+
+<h1>sara</h1>
+<h1>sara</h1>
+
+            <Features />
+            
+        </div>
+    );
 }
 
 export default Sara;
